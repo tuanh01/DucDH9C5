e086dda8544ec391c3c1541938da4bf5cae2e8d7
tu <tu@gmail.com>
2022-09-27 20:08:58 +0700

    duccode

---
+++ demo/src/demo/Demo.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demo;
+
+import java.util.Scanner;
+
+
+public class Demo {
+
+    public static void main(String[] args) {
+//        Scanner p = new Scanner(System.in);
+//        System.out.println("Hello word");
+//        System.out.println("Nhập họ tên:");
+//        String hoten= p.nextLine();
+//        System.out.println("Nhập điểm");
+//        float dtb = p.nextFloat();
+//        System.out.printf("%s đạt %.1f điểm", hoten, dtb);
+        Scanner scanner = new Scanner(System.in);
+        int a = 5;
+        int b = 8;
+        int c = a +b;
+        System.out.println("Tổng" +c);
+   
+                
+        
+    }
+    
+}
